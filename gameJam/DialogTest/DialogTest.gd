bf1df6d5c0eefa421076f73a50bc9bec243e751a
pmmv999 <39533241+pmmv999@users.noreply.github.com>
2021-04-03 00:47:10 +0000

    Add mercant chat

--- gameJam/DialogTest/DialogTest.gd
+++ gameJam/DialogTest/DialogTest.gd
@@ -1,19 +1,27 @@
 extends Node2D
 
+var money = 101;
+
 var newDialog;
 
 func _ready():
-	newDialog = Dialogic.start("TestTimeline");
-	newDialog.connect("dialogic_signal", self, "_on_Dialogic_Signal");
-	newDialog.connect("event_start", self, "_on_Event_Start")
-	$CanvasLayer.add_child(newDialog);
+	_on_startDialog("OpenShop");
 
 func _on_Dialogic_Signal(event):
 	print(event);
 
 func _on_Event_Start(action, event):
-	if(action == "choice"):
-		print(action);
-		print(event.choice)
-		print(event.question_id)
+	pass
+#	print(action);
+#	if(action == "choice"):
+##		print(action);
+##		print(event.choice)
+##		print(event.question_id)
 
+func _on_startDialog(timeline):
+	Dialogic.set_variable("money", money);
+	
+	newDialog = Dialogic.start(timeline);
+	newDialog.connect("dialogic_signal", self, "_on_Dialogic_Signal");
+	newDialog.connect("event_start", self, "_on_Event_Start")
+	$CanvasLayer.add_child(newDialog);
