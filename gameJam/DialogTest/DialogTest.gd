e61fb0b56679cdca80fd967e791fa9c293b032d7
pmmv999 <39533241+pmmv999@users.noreply.github.com>
2021-04-03 03:05:25 +0000

    First phase Dialogic

--- gameJam/DialogTest/DialogTest.gd
+++ gameJam/DialogTest/DialogTest.gd
@@ -1,15 +1,31 @@
 extends Node2D
 
-var money = 101;
+const ShopC = preload("res://DialogTest/Shop.gd");
 
-var newDialog;
+var money = 10000;
+
+var dialog;
+
+var O2BottleLevel = 0;
+var lampLevel = 0;
+var diveSuitLevel = 0;
+
+var shop;
 
 func _ready():
+	shop = ShopC.new();
 	_on_startDialog("OpenShop");
 
 func _on_Dialogic_Signal(event):
 	print(event);
-
+	if(event == "UpgradeO2Bottle"):
+		O2BottleLevel += 1;
+	if(event == "UpgradeLamp"):
+		lampLevel += 1;
+	if(event == "UpgradeDiveSuit"):
+		diveSuitLevel += 1;
+	setDialogVariables();
+	
 func _on_Event_Start(action, event):
 	pass
 #	print(action);
@@ -19,9 +35,18 @@ func _on_Event_Start(action, event):
 ##		print(event.question_id)
 
 func _on_startDialog(timeline):
-	Dialogic.set_variable("money", money);
+	setDialogVariables();
 	
-	newDialog = Dialogic.start(timeline);
-	newDialog.connect("dialogic_signal", self, "_on_Dialogic_Signal");
-	newDialog.connect("event_start", self, "_on_Event_Start")
-	$CanvasLayer.add_child(newDialog);
+	#dialog = Dialogic.start(timeline);
+	dialog = Dialogic.start(timeline, false, "res://addons/dialogic/Dialog.tscn", false)
+	dialog.connect("dialogic_signal", self, "_on_Dialogic_Signal");
+	dialog.connect("event_start", self, "_on_Event_Start")
+	$CanvasLayer.add_child(dialog);
+	
+func setDialogVariables():
+	print(Dialogic.get_definitions())
+	Dialogic.set_variable("Money", money)
+	Dialogic.set_variable("O2Bottle_Max_Level", O2BottleLevel == 3);
+	Dialogic.set_variable("O2Bottle_Upgrade_Price", shop.upgradePrices[O2BottleLevel]);
+	Dialogic.set_variable("Lamp_Upgrade_Price", shop.upgradePrices[lampLevel]);
+	Dialogic.set_variable("DiveSuit_Upgrade_Price", shop.upgradePrices[diveSuitLevel]);
---
+++ gameJam/DialogTest/Shop.gd
@@ -0,0 +1,2 @@
+enum products {O2BOTTLE,LAMP,DIVESUIT}
+var upgradePrices = [1000, 3000, 10000, INF];
