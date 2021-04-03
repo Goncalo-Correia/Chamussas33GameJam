ffc00d670cce72e2d6da8b8730b795864936eb20
pmmv999 <39533241+pmmv999@users.noreply.github.com>
2021-04-03 12:16:26 +0000

    Add font

--- gameJam/DialogTest/DialogTest.gd
+++ gameJam/DialogTest/DialogTest.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-const ShopC = preload("res://DialogTest/Shop.gd");
-
-var money = 10000;
+var money = 2000;
+var upgradePrices = [1000, 3000, 10000, INF];
 
 var dialog;
 
@@ -13,12 +12,12 @@ var diveSuitLevel = 0;
 var shop;
 
 func _ready():
-	shop = ShopC.new();
 	_on_startDialog("OpenShop");
 
 func _on_Dialogic_Signal(event):
 	print(event);
 	if(event == "UpgradeO2Bottle"):
+		money -= shop.upgradePrices[O2BottleLevel];
 		O2BottleLevel += 1;
 	if(event == "UpgradeLamp"):
 		lampLevel += 1;
@@ -27,28 +26,24 @@ func _on_Dialogic_Signal(event):
 	setDialogVariables();
 	
 func _on_Event_Start(action, event):
-	pass
-#	print(action);
-#	if(action == "choice"):
-##		print(action);
-##		print(event.choice)
-##		print(event.question_id)
+	print("ACTION:");
+	print(action)
+	print("EVENT:")
+	print(event)
 
 func _on_startDialog(timeline):
-	setDialogVariables();
-	
-	#dialog = Dialogic.start(timeline);
 	dialog = Dialogic.start(timeline, false, "res://addons/dialogic/Dialog.tscn", false)
+	setDialogVariables();
 	dialog.connect("dialogic_signal", self, "_on_Dialogic_Signal");
 	dialog.connect("event_start", self, "_on_Event_Start")
 	$CanvasLayer.add_child(dialog);
 	
 func setDialogVariables():
-	print(Dialogic.get_definitions())
-	Dialogic.set_variable("Money", money)
+	Dialogic.set_variable("money", money)
 	Dialogic.set_variable("O2Bottle_Max_Level", O2BottleLevel == 3);
-	Dialogic.set_variable("O2Bottle_Upgrade_Price", shop.upgradePrices[O2BottleLevel]);
+	Dialogic.set_variable("O2Bottle_Upgrade_Price", upgradePrices[O2BottleLevel]);
 	Dialogic.set_variable("lamp_Max_Level", lampLevel == 3);
-	Dialogic.set_variable("lamp_Upgrade_Price", shop.upgradePrices[lampLevel]);
+	Dialogic.set_variable("lamp_Upgrade_Price", upgradePrices[lampLevel]);
 	Dialogic.set_variable("diveSuit_Max_Level", diveSuitLevel == 3);
-	Dialogic.set_variable("diveSuit_Upgrade_Price", shop.upgradePrices[diveSuitLevel]);
+	Dialogic.set_variable("diveSuit_Upgrade_Price", upgradePrices[diveSuitLevel]);
+#	print(Dialogic.get_definitions());
