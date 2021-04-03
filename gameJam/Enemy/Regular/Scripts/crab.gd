7a9a63262e43f1587425966b19bb22292668bf96
Luis Sousa <luissousa@HUAWEI_P_smart-548ec2239b.localdomain>
2021-04-03 15:34:18 +0000

    last

--- gameJam/Enemy/Regular/Scripts/crab.gd
+++ gameJam/Enemy/Regular/Scripts/crab.gd
@@ -14,18 +14,26 @@ onready var dead_timer = $DeadTimer
 
 onready var animation_player = $AnimationPlayer
 
+var patrol_timer_running = false
+
 func _ready():
 	rng = RandomNumberGenerator.new()
 	curr_state = STATES.PATROLING
 	detection_area.connect("body_entered", self,"_on_body_entered")
 	detection_area.connect("body_exited", self, "_on_body_exited")
 	
+	patrolling_timer.connect("timeout", self, "_on_patrol_end")
+	idle_timer.connect("timeout",self,"_on_idle_end")
+	
 func _physics_process(delta):
 	match curr_state:
 		STATES.IDLE:
+			animation_player.play("idle")
 			pass
 		STATES.PATROLING:
-			patrol_movement(delta)
+			if not patrol_timer_running:
+				patrol_movement(delta)
+				patrol_timer_running = true
 			pass
 		STATES.FLEEING:
 			pass
@@ -56,5 +64,12 @@ func _on_body_exited(body):
 	return
 	curr_state = STATES.IDLE
 	pass
+	
+func _on_patrol_end():
+	curr_state = STATES.IDLE
+	patrol_timer_running = false
+	
+func _on_idle_end():
+	curr_state = STATES.PATROLING
 
 
